1c54b8170e90a4e81d30932430f10abe46fea430
saroj kaul <sarojmahi73@gmail.com>
2020-10-30 21:54:20 +0100

    adding files

--- src/Main.java
+++ src/Main.java
@@ -27,23 +27,37 @@ public class Main {
         squareArea(4);
 
         /*Bonus points sloution*/
+        /*could you please tell me the right way of doing it
+        i think i done this with the repition */
 
-        int[] intArray = new int[2];
+        int[] intArray = new int[6];
+
         Scanner input = new Scanner(System.in);
 
-            for (int i = 0; i < 2; i++) {
-                System.out.println("Please enter a number width & height:");
-                intArray[i] = input.nextInt();
+        for (int i = 0; i < 6; i++)
 
-
+        {
+            System.out.println("Please enter a number width & height:");
+            intArray[i] = input.nextInt();
         }
-        for (int i = 0; i<=2 ;i++) {
-            int arearec = intArray[0]*intArray[1];
-            System.out.println(arearec +"cm");
+
+                for (int i = 0; i < 1; i++)
+                    {
+                    int arearec1 = intArray[0] * intArray[1];
+                    int arearec2 = intArray[2] * intArray[3];
+                    int arearec3 = intArray[4] * intArray[5];
+                    System.out.println(arearec1 + "cm");
+                    System.out.println(arearec2 + "cm");
+                    System.out.println(arearec3 + "cm");
+                }
+            }
         }
-    }
 
 
-}
+
+
+
+
+
 
 
